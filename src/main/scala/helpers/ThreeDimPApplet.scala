ad1eff3355e32cc3ee435df6f7982cc81c5e876a
Ethan Petuchowski <etahnp@gmail.com>
2020-03-25 22:30:49 -0400

    add VennColors experiment

--- src/main/scala/helpers/ThreeDimPApplet.scala
+++ src/main/scala/helpers/ThreeDimPApplet.scala
@@ -1,11 +1,27 @@
 package helpers
-import processing.core.{PApplet, PConstants}
+import processing.core.{PApplet, PConstants, PVector}
 
+import scala.collection.mutable
 import scala.concurrent.duration.FiniteDuration
 
 /** Created 3/25/20 12:08 AM
  */
+//noinspection TypeAnnotation
 trait ThreeDimPApplet extends PApplet {
+
+  val gameObjects = mutable.ArrayBuffer.empty[GameObject]
+
+  trait GameObject {
+    def draw(): Unit = ()
+    def tick(): Unit = ()
+  }
+
+  /** Default implementation. Override as desired. */
+  override def draw(): Unit = {
+    darkBackground()
+    gameObjects.foreach(_.draw())
+    gameObjects.foreach(_.tick())
+  }
 
   override def settings(): Unit = fullScreen(PConstants.P3D)
 
@@ -18,7 +34,7 @@ trait ThreeDimPApplet extends PApplet {
   def translate(x: Double, y: Double, z: Double): Unit =
     ThreeDimPApplet.super.translate(x.toFloat, y.toFloat, z.toFloat)
 
-  def darkBackground(): Unit = background(15f)
+  def darkBackground(): Unit = background(0f)
 
   final protected def fromTheCenter(v: => Unit): Unit = {
     pushMatrix()
@@ -44,17 +60,18 @@ trait ThreeDimPApplet extends PApplet {
     }
   }
 
-  object Green extends Rgb(0, 255, 0)
-  object Black extends Rgb(0, 0, 0)
+  object Green extends Rgba(0, 100, 0)
+  object Black extends Rgba(0, 0, 0)
+  object White extends Rgba(100, 100, 100)
 
-  sealed case class Rgb(r: Float, g: Float, b: Float) extends Color {
+  sealed case class Rgba(r: Float, g: Float, b: Float, a: Float = 100) extends Color {
     override def stroke(): Unit = {
-      colorMode(PConstants.RGB)
-      ThreeDimPApplet.this.stroke(r, g, b)
+      colorMode(PConstants.RGB, 100)
+      ThreeDimPApplet.this.stroke(r, g, b, a)
     }
     override def fill(): Unit = {
-      colorMode(PConstants.RGB)
-      ThreeDimPApplet.this.fill(r, g, b)
+      colorMode(PConstants.RGB, 100)
+      ThreeDimPApplet.this.fill(r, g, b, a)
     }
   }
 
@@ -67,4 +84,29 @@ trait ThreeDimPApplet extends PApplet {
       }
     }
   }
+
+  object PVector {
+    def apply(x: Double, y: Double, z: Double = 0) =
+      new PVector(x.toFloat, y.toFloat, z.toFloat)
+  }
+
+  case class Rotation(period: FiniteDuration) extends GameObject {
+    private var rotationFraction = 0D
+    val millerPerPeriod = 1.0 / period.toMillis
+    private var lastTime = millis()
+    gameObjects += this
+
+    override def tick(): Unit = {
+      val elapsedTime = millis() - lastTime
+      // TODO is this right? can't think right now.
+      //  It should "work" (rotate) anyhow, just not necessarily at the right speed.
+      rotationFraction += elapsedTime.toDouble / period.toMillis
+      lastTime = millis()
+    }
+
+    def curRadians(): Float = angleFractionToRadians(rotationFraction)
+  }
+
+  def angleFractionToRadians(angleFraction: Double): Float =
+    ((angleFraction - (math floor angleFraction)) * math.Pi * 2).toFloat
 }
---
+++ src/main/scala/mygo/VennColors.scala
@@ -0,0 +1,55 @@
+package mygo
+
+import helpers.{Runner, ThreeDimPApplet}
+import processing.core.PConstants
+
+import scala.concurrent.duration._
+import scala.language.postfixOps
+
+/** Created 3/24/20 10:45 PM
+ */
+class VennColors extends ThreeDimPApplet {
+
+  private val intensity = 60
+
+  gameObjects ++= Seq(
+    StaticCircle(color = Rgba(intensity, 0, 0, intensity), givenDegrees = 0),
+    StaticCircle(color = Rgba(0, intensity, 0, intensity), givenDegrees = 120),
+    StaticCircle(color = Rgba(0, 0, intensity, intensity), givenDegrees = 240),
+    SpinningCircle(color = Rgba(intensity, intensity, intensity, intensity))
+  )
+
+  trait VennCircle extends GameObject {
+
+    def curRadians: Double
+    def color: Color
+
+    private val overlap = 100
+    private val topLeft = PVector(x = -overlap, y = -overlap)
+    private val Width = 400
+    private val Height = 400
+
+    override def draw(): Unit = {
+      fromTheCenter {
+        rotateZ(curRadians.toFloat)
+        color.fill()
+        White.stroke()
+        ellipseMode(PConstants.CORNER)
+        ellipse(topLeft.x, topLeft.y, Width, Height)
+      }
+    }
+  }
+
+  case class StaticCircle(color: Color, givenDegrees: Double) extends VennCircle {
+    override def curRadians: Double = math.toRadians(givenDegrees)
+  }
+
+  case class SpinningCircle(color: Color) extends VennCircle {
+    private val rotation = Rotation(period = 3 seconds)
+    override def curRadians: Double = rotation.curRadians()
+  }
+}
+
+object VennColors extends Runner {
+  override def pAppletClass: Class[_] = classOf[VennColors]
+}
