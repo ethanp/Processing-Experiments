39902b8bf33347db89bd2c2b95dc7df4b5f188dc
Ethan Petuchowski <etahnp@gmail.com>
2020-04-04 10:45:34 -0400

    drew a basic spiral

--- src/main/scala/mygo/SpiralDevEnv.scala
+++ src/main/scala/mygo/SpiralDevEnv.scala
@@ -8,8 +8,27 @@ import helpers.{MyPApplet, Runner}
  * Created 4/3/20 11:04 PM
  */
 class SpiralDevEnv extends MyPApplet {
+  override def settings(): Unit = size(700, 700)
+
   override def draw(): Unit = {
-    // TODO fill this in.
+    blackBackground()
+    fromTheCenter {
+      var radius: Float = 1f
+      for (degrees <- 0 until (360 * 4)) {
+        colors.set(
+          fill = colors.Hsb(radius % 100),
+          stroke = colors.Empty
+        )
+        val angle = math.toRadians(degrees)
+        ellipse(
+          /*x*/ math.cos(angle).toFloat * radius,
+          /*y*/ math.sin(angle).toFloat * radius,
+          /*w*/ 7,
+          /*h*/ 7
+        )
+        radius += .2f
+      }
+    }
   }
 }
 
