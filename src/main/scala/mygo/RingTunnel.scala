d44221ebc94611f6a4306b52cfcec8251023d3cf
Ethan Petuchowski <etahnp@gmail.com>
2020-04-12 13:12:06 -0400

    convert RingTunnel to use GameObject

--- src/main/scala/mygo/RingTunnel.scala
+++ src/main/scala/mygo/RingTunnel.scala
@@ -5,20 +5,18 @@ import processing.core.PConstants
 /** Created 4/12/20 1:00 PM
  */
 class RingTunnel extends ThreeDimPApplet {
-  // TODO convert this to use [[GameObject]]s
-  // Create the rings
-  override def draw(): Unit = {
-    fromTheCenter {
-      for (i <- 0 until 30) {
-        colors.Current.update(
-          fill = colors.Solarized.Orange,
-          stroke = colors.Solarized.White
-        )
-        translate(0, 0, i * 10)
-        ellipseMode(PConstants.RADIUS)
-        // Center and radius
-        ellipse(0, 0, i * 10, i * 10)
-      }
+  for (i <- 0 until 30) gameObjects += new Ring(i)
+
+  class Ring(i: Int) extends GameObject {
+    override def draw(): Unit = {
+      colors.Current.update(
+        fill = colors.Solarized.Orange,
+        stroke = colors.Solarized.White
+      )
+      translate(0, 0, i * 10)
+      ellipseMode(PConstants.RADIUS)
+      // Center and radius
+      ellipse(0, 0, i * 10, i * 10)
     }
   }
 }
