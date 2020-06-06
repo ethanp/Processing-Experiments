fdaee1f3af5152f56e995011bbd40c1c9944b151
Ethan Petuchowski <etahnp@gmail.com>
2020-06-06 01:04:39 -0400

    cleanup PixelComparator.scala

--- src/main/scala/helpers/PixelComparator.scala
+++ src/main/scala/helpers/PixelComparator.scala
@@ -4,27 +4,16 @@ import processing.core.PImage
 /** Created 4/5/20 5:40 PM
  */
 object PixelComparator {
-  // LowPriorityTodo can we write unit test for this somehow?
-  //
-  //  The problem is that we probably need to have a PApplet context available
-  //  to be able to create the PImages. Though I could dig into what the PApplet
-  //  method actually does and maybe it calls something static? But probs not.
-  def areEquivalent(
-    aImg: PImage,
-    bImg: PImage,
-  )(
-    implicit myPApplet: MyPApplet
-  ): Boolean = {
+  // We'd probably need to have a PApplet context available to be able to create the
+  // PImages in a unit test....
+  def areEquivalent(aImg: PImage, bImg: PImage)(implicit myPApplet: MyPApplet): Boolean = {
     aImg.loadPixels()
     bImg.loadPixels()
-    if (aImg.pixels.length != bImg.pixels.length) {
+
+    if (aImg.pixels.length != bImg.pixels.length)
       return false
-    }
-    for ((a, b) <- aImg.pixels.zip(bImg.pixels)) {
-      if (a != b) {
-        return false
-      }
-    }
-    true
+
+    // NB: this is actually efficient since zip is lazy and exists is eager.
+    !aImg.pixels.zip(bImg.pixels).exists { case (a, b) => a != b }
   }
 }
